8fbcf94483b6255618ec19d5a0f485dc5c181b55
KonovalovDM <kdm232006@gmail.com>
2024-09-16 21:13:14 +0300

    add new code in main

--- main.py
+++ main.py
@@ -4,5 +4,13 @@ a = 10
 b = 20
 c = a + b
 print(c)          # печать произведения суммы
+# Функция добавления задачи в План, которая запускается по нажатию кнопки
+def task_writing():
+    task = entry.get()
+    if task != '':
+        task_listBox.insert(tk.END, task)
+        entry.delete(0, tk.END)
+        label.config(text=f'\nВнесите задачу в План\nЗадача записана, можно внести следующую\n')
+        save_tasks()  # Сохраняем задачи после добавления новой
 
 
